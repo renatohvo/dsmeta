59321fd9fb8e706fe45c71475e6898edb1f9a207
renatohvo <renatohvo@gmail.com>
2022-07-11 18:54:24 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import logo from '../../assets/img/logo.svg'
-import './style.css'
+import logo from '../../assets/img/logo.svg';
+import './style.css';
 
 function Header() {
     return (
@@ -15,4 +15,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,16 +1,34 @@
-import NotificationButton from '../NotificationButton'
-import './style.css'
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import NotificationButton from '../NotificationButton';
+import './style.css';
 
 function SalesCard() {
+
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const[minDate, setMinDate] = useState(min);
+  const[maxDate, setMaxDate] = useState(new Date());
+
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
-          <input className="dsmeta-form-control" type="text" />
+          <DatePicker
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
         <div className="dsmeta-form-control-container">
-          <input className="dsmeta-form-control" type="text" />
+          <DatePicker
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
 
@@ -29,7 +47,7 @@ function SalesCard() {
           </thead>
           <tbody>
             <tr>
-              <td className="show992">#341</td>
+              <td className="show992">#001</td>
               <td className="show576">08/07/2022</td>
               <td>Jamile</td>
               <td className="show992">15</td>
@@ -42,7 +60,7 @@ function SalesCard() {
               </td>
             </tr>
             <tr>
-              <td className="show992">#342</td>
+              <td className="show992">#002</td>
               <td className="show576">08/07/2022</td>
               <td>Renato</td>
               <td className="show992">15</td>
@@ -55,7 +73,7 @@ function SalesCard() {
               </td>
             </tr>
             <tr>
-              <td className="show992">#343</td>
+              <td className="show992">#003</td>
               <td className="show576">08/07/2022</td>
               <td>Felipe</td>
               <td className="show992">15</td>
@@ -76,4 +94,4 @@ function SalesCard() {
   )
 }
 
-export default SalesCard
+export default SalesCard;
