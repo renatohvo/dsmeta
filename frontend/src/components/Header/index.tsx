a8e0d6a35c2554de996aced03adcecd9c64e4c1c
Renato Henrique <renatohvo@mxlinux>
2022-07-11 18:06:50 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>RHVOMeta</h1>
+                <p>
+                    Desenvolvido por <a href="https://linkedin.com/in/renatohvo">@renatohvo</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
